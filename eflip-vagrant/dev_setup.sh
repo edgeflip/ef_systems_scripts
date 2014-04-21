59b27bce2474949f1c31b0103ffae39576e96b33
Mark Rogers <f4nt@f4ntasmic.com>
2014-04-21 10:39:38 -0500

    pointing at main repo

--- eflip-vagrant/dev_setup.sh
+++ eflip-vagrant/dev_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export MYSQL_ROOT_PW=root
-git clone git@github.com:f4nt/edgeflip.git
+git clone git@github.com:edgeflip/edgeflip.git
 export DEBIAN_FRONTEND=noninteractive 
 cd edgeflip 
 fab build 
